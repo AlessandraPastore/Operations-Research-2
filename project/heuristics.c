44afeb2f7bc3f2f36798e571e45b63bde6da47d1
matte <matteo.pozzer@studenti.unipd.it>
2023-03-16 09:39:22 +0100

    add opt_2

--- project/heuristics.c
+++ project/heuristics.c
@@ -2,11 +2,15 @@
 int greedy(instance *inst,int startNode);
 int grasp(instance *inst,int startNode);
 int extra_mileage(instance *inst);
+int opt_2(instance *inst);
 
 int heuristic(instance *inst){
     if(strcmp(inst->heuristic, "GREEDY") == 0)           return greedy(inst,0);
     if(strcmp(inst->heuristic, "GREEDYGRASP") == 0)      return grasp(inst,0);
     if(strcmp(inst->heuristic, "EXTRA_MILEAGE") == 0)    return extra_mileage(inst);
-
+    if(strcmp(inst->heuristic, "2_OPT") == 0)   {
+        grasp(inst,0);
+        return opt_2(inst);
+     }
     return 1;
 }
---
+++ project/opt_2.c
@@ -0,0 +1,40 @@
+#include "tsp.h"
+void reverse(instance *inst,int swap1,int swap2)
+{
+    int* old = (int*)malloc(inst->nnodes * sizeof(int));
+    memcpy(old,inst->best_sol,sizeof(int)*inst->nnodes);
+
+
+    
+    free(old);
+
+}
+int opt_2(instance *inst){
+    double t=second();
+    
+    double delta=INFBOUND;
+    int swap1=-1,swap2=-1;
+    do{
+        for(int i=0;i<inst->nnodes;i++)
+            for(int j=0;j<inst->nnodes;j++)
+            {
+                if(i!=j){
+                    
+                    double deltaTemp = get_cost(i,j,inst)  +  get_cost(inst->best_sol[j],inst->best_sol[i],inst) -  (get_cost(i,inst->best_sol[i],inst)  +  get_cost(j,inst->best_sol[j],inst));
+                    
+                    if(deltaTemp<delta)
+                    {
+                        delta=deltaTemp;
+                        swap1=i;
+                        swap2=j;
+                    }
+                }
+                
+            }
+        reverse(inst,swap1,swap2);
+
+    }while(second()-t<inst->timelimit);
+    
+    
+    return 0;
+}
