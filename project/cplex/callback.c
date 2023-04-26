0597d548f21c597b7ae9317e60e77305c29fff0c
matte <matteo.pozzer@studenti.unipd.it>
2023-04-26 17:23:35 +0200

    fixes

--- project/cplex/callback.c
+++ project/cplex/callback.c
@@ -11,7 +11,7 @@ static int CPXPUBLIC my_callback(CPXCALLBACKCONTEXTptr context, CPXLONG contexti
 	int mythread = -1; CPXcallbackgetinfoint(context, CPXCALLBACKINFO_THREADID, &mythread);
 	int mynode = -1; CPXcallbackgetinfoint(context, CPXCALLBACKINFO_NODECOUNT, &mynode);
 	double incumbent = CPX_INFBOUND; CPXcallbackgetinfodbl(context, CPXCALLBACKINFO_BEST_SOL, &incumbent);
-	if ( VERBOSE >= 10 ) printf(" ... callback at node %5d thread %2d incumbent %10.2lf\n", mynode,mythread,incumbent);
+	if (VERBOSE >= 10) printf(" ... callback at node %5d thread %2d incumbent %10.2lf\n", mynode, mythread, incumbent);
 
 	//vector with the current solutions
 	int* succ = (int*)malloc(inst->nnodes * sizeof(int));
@@ -61,85 +61,85 @@ static int CPXPUBLIC my_callback(CPXCALLBACKCONTEXTptr context, CPXLONG contexti
 
 		}
 	}
-	
-		free(succ);
-		free(comp);
-		free(xstar);
-		return 0;
-	
+
+	free(succ);
+	free(comp);
+	free(xstar);
+	return 0;
+
 }
 
-int callback_sec(instance * inst, CPXENVptr env, CPXLPptr lp) {
+int callback_sec(instance* inst, CPXENVptr env, CPXLPptr lp) {
 
-		printf("--- starting B&C via callback ---\n");
+	printf("--- starting B&C via callback ---\n");
 
-		int ncols = CPXgetnumcols(env, lp);
-		inst->ncols = ncols;
-		//to store the solution
-		double* xstar = (double*)calloc(ncols, sizeof(double));
+	int ncols = CPXgetnumcols(env, lp);
+	inst->ncols = ncols;
+	//to store the solution
+	double* xstar = (double*)calloc(ncols, sizeof(double));
 
-		CPXsetdblparam(env, CPX_PARAM_TILIM,  inst->timelimit);
-		CPXLONG contextid = CPX_CALLBACKCONTEXT_CANDIDATE; // ... means lazyconstraints
-		if (CPXcallbacksetfunc(env, lp, contextid, my_callback, inst)) print_error("CPXcallbacksetfunc() error");
+	CPXsetdblparam(env, CPX_PARAM_TILIM, inst->timelimit);
+	CPXLONG contextid = CPX_CALLBACKCONTEXT_CANDIDATE; // ... means lazyconstraints
+	if (CPXcallbacksetfunc(env, lp, contextid, my_callback, inst)) print_error("CPXcallbacksetfunc() error");
 
-		int error = CPXmipopt(env, lp);
-		if (error)
-		{
-			printf("CPX error code %d\n", error);
-			print_error("CPXmipopt() error");
-		}
-		//vector with the current solutions
-		int* succ = (int*)malloc(inst->nnodes * sizeof(int));
-
-		//components vector
-		int* comp = (int*)malloc(inst->nnodes * sizeof(int));
-		int ncomp = 0;
-		if (CPXgetx(env, lp, xstar, 0, ncols - 1)) {
-
-			if (inst->zbest != -1) {
-				printf("----- Terminated before convergence -----\n");
-				printf("BEST SOLUTION FOUND\nCOST: %f\n", inst->zbest);
-				plot(inst, succ, "benders");
-				return 0;
-			}
-			else print_error("CPXgetx() error");
-		}
-		build_sol(xstar, inst, succ, comp, &ncomp);
-		
-		for (int i = 0; i < inst->nnodes; i++)
-		{
-			for (int j = i + 1; j < inst->nnodes; j++)
-			{
-				if (xstar[xpos(i, j, inst)] > 0.5) printf("  ... x(%3d,%3d) = 1\n", i + 1, j + 1);
-			}
-		}
-		double objval = -1;
-		if (CPXgetobjval(env, lp, &objval)) {
-			if (inst->zbest != -1) {
-				printf("----- Terminated before convergence -----\n");
-				printf("BEST SOLUTION FOUND\nCOST: %f\n", inst->zbest);
-				plot(inst, succ, "benders");
-				return 0;
-			}
-			else print_error("CPXgetx() error");
-			}
-
-		if (VERBOSE >= 10) {
-			if (checkSol(inst, succ)) return 1;
-			if (checkCost(inst, succ, objval)) return 1;
-		}
-
-			//if current cost is better, update best solution
-		if (inst->zbest == -1 || inst->zbest > objval) {
-			updateSol(inst, objval, succ);
-		}
-
-		if (VERBOSE >= 1) printf("BEST SOLUTION FOUND\nCOST: %f\n", inst->zbest);
-			plot(inst, succ, "callback_sec");
-
-		
-		free(succ);
-		free(comp);
-		free(xstar);
-		return 0;
+	int error = CPXmipopt(env, lp);
+	if (error)
+	{
+		printf("CPX error code %d\n", error);
+		print_error("CPXmipopt() error");
 	}
+	//vector with the current solutions
+	int* succ = (int*)malloc(inst->nnodes * sizeof(int));
+
+	//components vector
+	int* comp = (int*)malloc(inst->nnodes * sizeof(int));
+	int ncomp = 0;
+	if (CPXgetx(env, lp, xstar, 0, ncols - 1)) {
+
+		if (inst->zbest != -1) {
+			printf("----- Terminated before convergence -----\n");
+			printf("BEST SOLUTION FOUND\nCOST: %f\n", inst->zbest);
+			plot(inst, succ, "benders");
+			return 0;
+		}
+		else print_error("CPXgetx() error");
+	}
+	build_sol(xstar, inst, succ, comp, &ncomp);
+
+	for (int i = 0; i < inst->nnodes; i++)
+	{
+		for (int j = i + 1; j < inst->nnodes; j++)
+		{
+			if (xstar[xpos(i, j, inst)] > 0.5) printf("  ... x(%3d,%3d) = 1\n", i + 1, j + 1);
+		}
+	}
+	double objval = -1;
+	if (CPXgetobjval(env, lp, &objval)) {
+		if (inst->zbest != -1) {
+			printf("----- Terminated before convergence -----\n");
+			printf("BEST SOLUTION FOUND\nCOST: %f\n", inst->zbest);
+			plot(inst, succ, "benders");
+			return 0;
+		}
+		else print_error("CPXgetx() error");
+	}
+
+	if (VERBOSE >= 10) {
+		if (checkSol(inst, succ)) return 1;
+		if (checkCost(inst, succ, objval)) return 1;
+	}
+
+	//if current cost is better, update best solution
+	if (inst->zbest == -1 || inst->zbest > objval) {
+		updateSol(inst, objval, succ);
+	}
+
+	if (VERBOSE >= 1) printf("BEST SOLUTION FOUND\nCOST: %f\n", inst->zbest);
+	plot(inst, succ, "callback_sec");
+
+
+	free(succ);
+	free(comp);
+	free(xstar);
+	return 0;
+}
